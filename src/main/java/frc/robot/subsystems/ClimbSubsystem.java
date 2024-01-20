fff619e5fe7163d4c9ba72e952d44c13d18816b2
SPP Robotics Team <spprobotics@gmail.com>
2024-01-20 14:51:27 -0500

    Modifying Climbing code

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,6 +1,21 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import frc.robot.Constants;
+
 public class ClimbSubsystem
 {
-    
+    private CANSparkMax climbMotorLeft = new CANSparkMax(Constants.ClimbConstants.climbMotorLeftID, MotorType.kBrushless);
+    private CANSparkMax climbMotorRight = new CANSparkMax(Constants.ClimbConstants.climbMotorRightID, MotorType.kBrushless);
+
+    // This will be called by triggers on the operator controller
+    public void setClimbMotors(double desiredSpeed)
+    {
+        climbMotorLeft.set(desiredSpeed);
+        climbMotorRight.set(desiredSpeed);
+    }
+
+
 }
