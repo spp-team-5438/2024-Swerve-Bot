e34d75fd5f0c9ed3fa31dc3fb6ad2f9a9e0c94ee
Phantomforce260 <120600016+Phantomforce260@users.noreply.github.com>
2024-01-22 15:28:09 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -35,7 +35,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    LEDSubsystem LED = new LEDSubsystem();
+    LEDSubsystem LED = new LEDSubsystem(0, 1);
   }
 
   /**
