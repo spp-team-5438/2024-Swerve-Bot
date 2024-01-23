e7a2145f37ff4e9707e434be591f19662b2f94de
Phantomforce260 <120600016+Phantomforce260@users.noreply.github.com>
2024-01-23 17:36:25 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
@@ -97,6 +98,6 @@ public class RobotContainer {
     public Command followSamplePath(String pathName)
     {
         PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
-        return AutoBuilder.followPath(path);
+        return Commands.runOnce(() -> s_Swerve.resetOdometry(path.getPreviewStartingHolonomicPose())).andThen(AutoBuilder.followPath(path));
     }
 }
