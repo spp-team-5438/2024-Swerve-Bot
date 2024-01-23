f9ed2c152dbfea6384a57d5d75a9770fc7a67b6d
Harrison <hdiambrosio@gmail.com>
2024-01-23 13:26:15 -0500

    Added aprilTag Location for testing 3d location purposes

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -8,27 +8,31 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 
 public class Limelight {
-    NetworkTable table;
-    NetworkTableEntry tx, ty, ta;
-    double x, y, area;
+    double[] aprilTagPosition;
+    // NetworkTableEntry tx, ty, tz;
+    // double x, y, z;
     
     public Limelight() {
         updateValues();
     }
 
     public void updateValues() {
-        table = NetworkTableInstance.getDefault().getTable("limelight");
+        // table = NetworkTableInstance.getDefault().getTable("limelight");
 
-        tx = table.getEntry("tx");
-        ty = table.getEntry("ty");
-        ta = table.getEntry("ta");
+        // tx = table.getEntry("MegaTag BotPose TX");
+        // ty = table.getEntry("MegaTag BotPose TY");
+        // tz = table.getEntry("MegaTag BotPose TZ");
+        
 
-        x = tx.getDouble(69.69);
-        y = ty.getDouble(69.69);
-        area = ta.getDouble(69.69);
+        // x = tx.getDouble(69.69);
+        // y = ty.getDouble(69.69);
+        // z = tz.getDouble(69.69);
 
-        SmartDashboard.putNumber("LimelightX", x);
-        SmartDashboard.putNumber("LimelightY", y);
-        SmartDashboard.putNumber("LimelightArea", area);
+        // SmartDashboard.putNumber("LimelightX", x);
+        // SmartDashboard.putNumber("LimelightY", y);
+        // SmartDashboard.putNumber("LimelightZ", z);
+        aprilTagPosition = NetworkTableInstance.getDefault().getTable("limelight").getEntry("targetpose_cameraspace").getDoubleArray(new double[6]);
+        SmartDashboard.putNumberArray("targetpose_cameraspace", aprilTagPosition);
+        
     }
 }
